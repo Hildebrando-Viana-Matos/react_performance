46789101f1ecc608b8fdbd186947d381f7df6f0a
Hildebrando Viana Matos <hildocontato@gmail.com>
2021-06-08 11:58:27 -0300

    useMemo

--- components/SearchResults.tsx
+++ components/SearchResults.tsx
@@ -1,3 +1,4 @@
+import { useMemo } from "react";
 import { ProductItem } from "./ProductItem";
 
 interface SearchResultsProps {
@@ -9,11 +10,25 @@ interface SearchResultsProps {
 }
 
 export function SearchResults({ results }: SearchResultsProps) {
+  const totalPrice = useMemo(() => {
+    return results.reduce((total, product) => {
+      return total + product.price;
+    }, 0);
+  }, [results]);
+  // Toda fez que o resultado da minha busca mudar o calculo será refeito
   return (
     <div>
+      <h2>{totalPrice}</h2>
       {results.map((product) => {
         return <ProductItem product={product} />;
       })}
     </div>
   );
 }
+
+/*
+Quando usar o useMemo
+
+1. Cálculos pesados
+2. Igualdade referencial (quando passamos uma informação para um componente filho)
+*/
